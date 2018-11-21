42e09f2867a7cc35be29024522b0956afe243207
virandry <mvi@accordium.com>
2018-11-21 14:05:28 +0800

    try lib

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TextInput from "./lib/components/TextInput";
+
+ReactDOM.render(
+  <div>
+    <TextInput />
+  </div>,
+  document.getElementById("root")
+);
